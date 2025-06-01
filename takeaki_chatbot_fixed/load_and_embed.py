502bfa120c36405c2d9407bb7e06012ac786411f
Takeaki920 <pcskilltaker1725@gmail.com>
2025-06-01 14:31:53 +0900

    Update load_and_embed.py

--- takeaki_chatbot_fixed/load_and_embed.py
+++ takeaki_chatbot_fixed/load_and_embed.py
@@ -1,14 +1,15 @@
-
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.document_loaders import TextLoader
+from langchain_core.documents import Document
 from langchain.text_splitter import CharacterTextSplitter
 
 def load_vectorstore():
-    loader = TextLoader("book.txt", encoding="utf-8")
-    documents = loader.load()
+    with open("book.txt", "r", encoding="utf-8") as f:
+        text = f.read()
+
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    docs = text_splitter.split_documents(documents)
+    docs = text_splitter.split_documents([Document(page_content=text)])
+
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(docs, embeddings)
     return vectorstore
